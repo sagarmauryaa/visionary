ec279b756d0011daae16f913c34f4e4e00bc27d5
Sagar Maurya <sagar68742@gmail.com>
2024-12-22 01:31:47 +0530

    first commit

--- src/app/components/AnimatedSection.tsx
+++ src/app/components/AnimatedSection.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import SplitText from "../components/SplitText";
 
